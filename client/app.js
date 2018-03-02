28913fc37f107a961c8dbe45813a6cf0572ab4bc
borajimin <borajimin@gmail.com>
2018-03-02 10:42:08 -0800

    react set up

---
+++ client/app.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>React Chat</h1>
+        <button className="btn btn-default">
+          Join the Party Place
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
