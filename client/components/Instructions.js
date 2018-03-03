b205e66344490aa6796da1147eb72c6794122fc8
luchen01 <luchenpengnyc@gmail.com>
2018-03-02 21:18:04 -0800

    minor fix instructions

--- client/components/Instructions.js
+++ client/components/Instructions.js
@@ -27,11 +27,11 @@ class Instructions extends React.Component {
 
   render() {
     return (
-      <div style={{"flexDirection": "column", "display": "flex", "justifyContent": "center", "flex": "1"}}>
+      <div style={{"marginTop": "80px", "flexDirection": "column", "display": "flex", "justifyContent": "center", "flex": "1"}}>
         <div style = {instruction}>
           {/* <img src={BobaScript} alt="logo"></img> */}
           <h1>BobaScript Documentations</h1>
-          <p>BobaScript (BS) is a lightweight interpreted or JIT-compiled programming language.
+          <p><b>BobaScript (BS)</b> is a lightweight interpreted or JIT-compiled programming language.
         While it is most well-known as the scripting language for Horizons tests, many
         non-browser environments might not use it. BobaScript is a prototype-based,
         multi-paradigm, dynamic language, supporting object-oriented programming.
@@ -47,19 +47,20 @@ class Instructions extends React.Component {
             <div>
               <h1>Boba Class</h1>
               <div><h3>Properties:</h3>
-                value: value of Boba<br/>
+                value: value of Boba, it can be color, number, ingredients, flavors, etc.<br/>
                 xCoordinate: xCoordinate of Boba<br/>
                 yCoordinate: yCoordinate of Boba<br/>
               </div><br/>
               <div><h3>Methods:</h3>
                 move: Boba will move one unit.<br/>
-                turnLeft: Boba will turn to the left.<br/>
-                turnRight: Boba will turn to the right.<br/>
+                turnLeft: Boba will turn to the left, it will not move the Boba.<br/>
+                turnRight: Boba will turn to the right, it will not move the Boba.<br/>
               </div>
             </div>
             <div>
               <h1>Cup Class</h1>
               <div><h3>Properties:</h3>
+                value: the value of the cup, it can be color, number, ingredients, flavors, etc.
                 left: left coordinate of the cup.<br/>
                 right: right coordinate of the cup.<br/>
                 top: top coordinate of the cup.<br/>
